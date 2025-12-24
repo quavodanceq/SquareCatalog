55dfe42eb329289fe489db4740723aa81e748ea6
Kuat <80914126+quavodanceq@users.noreply.github.com>
2025-12-24 15:17:05 +0500

    Initial Commit

---
+++ SquareCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareCatalog
+//
+//  Created by Куат Оралбеков on 24.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
